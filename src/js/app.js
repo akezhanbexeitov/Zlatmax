69b82d598f15ce3b02bcf767906bd36df11ad1b7
Akezhan <akidulatini@gmail.com>
2022-09-29 14:59:29 +0800

    Complete header-top, logo, search, location and phone spoiler

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,16 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-/*
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+// ==================== Spoilers ====================
+const arrowDown = document.querySelector('._icon-arrow-down')
+const phoneList = document.querySelector('.phones-header__list')
+
+arrowDown.addEventListener('click', () => {
+    if (phoneList.style.maxHeight == 0) {
+        phoneList.style.padding = 15 + 'px'
+        phoneList.style.maxHeight = phoneList.scrollHeight + 30 + 'px';
+    } else {
+        phoneList.style.maxHeight = null;
+        phoneList.style.padding = 0 + 'px';
+    }
+})
